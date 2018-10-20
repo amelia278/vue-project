8e306a4aa1087eea75e1bef059fd962dd1414c37
Qu Amelia <33386378+amelia278@users.noreply.github.com>
2018-10-20 20:50:36 +0800

    add project files
    add project files

---
+++ src/app.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <mt-header fixed title=''>
+            <!--<mt-p>欢迎你{{name}}</mt-p>-->
+            <router-link to='' slot='right'>
+                <mt-button>登录</mt-button>
+            </router-link>
+            <router-link to='' slot='right'>
+                <mt-button>注册</mt-button>
+            </router-link>
+        </mt-header>
+        <!-- <nav-bar></nav-bar> -->
+        <router-view></router-view>
+        <!--footer-->
+        <router-link :to="{name:'home'}">
+            <!--<mt-tabbar></mt-tabbar>-->
+        </router-link>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                name:"yaya"
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
---
+++ src/components/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <mt-tabbar>
+            <mt-tab-item class='mt-tab-item' id='purchase'>
+                <img slot='icon' src='../../assets/images/purchase.png'>
+            </mt-tab-item>
+            <mt-tab-item class='mt-tab-item' id='audio'>
+                <!--<img slot='icon' src='../assets/images/purchase.png'>-->
+            </mt-tab-item>
+        </mt-tabbar>
+    </div>
+    
+</template>
+<script>
+    export default{
+        created(){}
+    }
+</script>
