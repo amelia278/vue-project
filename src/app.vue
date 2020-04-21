6e39ad2ac34ce687796a96cac79212ee1096b84c
qxy <qxy>
2020-04-21 21:16:51 +0800

    update editor

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,6 @@
 <template>
     <div>
-        <mt-header fixed title=''>
-            <!--<mt-p>欢迎你{{name}}</mt-p>-->
-            <router-link to='' slot='right'>
-                <mt-button>登录</mt-button>
-            </router-link>
-            <router-link to='' slot='right'>
-                <mt-button>注册</mt-button>
-            </router-link>
-        </mt-header>
-        <!-- <nav-bar></nav-bar> -->
         <router-view></router-view>
-        <!--footer-->
-        <!-- <router-link :to="{name:'home'}">
-        </router-link> -->
-        <foot-bar></foot-bar>
     </div>
 </template>
 <script>
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,14 +1,51 @@
 <template>
-    <div>
-        <!-- <mt-tabbar>
-            <mt-tab-item class='mt-tab-item' id='purchase'>
-                <img slot='icon' src='../../assets/images/purchase.png'>
-            </mt-tab-item>
-        </mt-tabbar> -->
+    <div class="home">
+        <vue-editor class="editor" v-model="content"></vue-editor>
+        <button class="btn" @click="sumbit()">
+            submit
+        </button>
     </div>   
 </template>
-<script>
-    export default{
-        created(){}
+<script type="text/babel">
+    import { VueEditor } from "vue2-editor";
+
+    export default {
+        name: 'home',
+
+        components: {
+            VueEditor
+        },
+
+        data() {
+            return {
+                content: null
+            }
+        },
+
+        methods: {
+            submit() {
+                // 接口 this.content 传入
+            }
+        }
     }
 </script>
+
+<style>
+    .home 
+        .editor {
+            position: relative;
+            margin: 0 auto;
+            max-width: 80vw;
+        }
+
+        .ql-container {
+            max-height: 600px;
+        }
+        
+        .btn {
+            margin-top: 30px;
+            margin-left: 200px;
+            height: 30px;
+            width: 100px;
+        }
+</style>
